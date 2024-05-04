ce7ec43bd3cccfa0de91fb55abbf3ccd3ea9870b
Limitation-tm <limitation-tm@gmail.com>
2024-05-04 16:50:16 +0400

    uploads

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
 import cors from "cors";
+import fs from "fs";
 
 import {
   loginValidation,
@@ -44,6 +45,9 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
     cb(null, "uploads");
   },
   filename: (_, file, cb) => {
